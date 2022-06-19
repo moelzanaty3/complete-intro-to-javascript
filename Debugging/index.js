f7e4aee9f1bcc4e272792bf2057c6359a73129eb
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2022-06-19 16:10:56 +0200

    feat: Javascript Debugging Strategies

---
+++ Debugging/index.js
@@ -0,0 +1,35 @@
+const button = document.querySelector("button");
+const todosContainer = document.getElementById("todos");
+
+function createTodo(todo) {
+  const todoElement = document.createElement("li");
+  todoElement.textContent = todo.title;
+  return todoElement;
+}
+
+async function fetchData() {
+  try {
+    const URL = "https://jsonplaceholder.typicode.com/todos";
+    const response = await fetch(URL);
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // change state of the button
+  button.innerText = "Loading...";
+  // clear previous todos
+  todosContainer.textContent = "";
+  // fetch new todos
+  const todos = await fetchData();
+  // update UI with new todos
+  const fragment = document.createDocumentFragment();
+  todos.map((todo) => {
+    fragment.appendChild(createTodo(todo));
+  });
+  todosContainer.appendChild(fragment);
+  // change state of the button after fetching new todos
+  button.innerText = "Get Todos";
+});
